0726053bf1b800f1e301de8f8810c1b575844a0e
sagar kumar yadav <sk5323912@gmail.com>
2023-10-04 10:45:45 +0530

    new commit

---
+++ login.js
@@ -0,0 +1,18 @@
+const { Selector } = require("testcafe");
+
+fixture`Group of tests / Test suite - Login Suite`
+  .page`https://dev.deepthought.education/login`;
+
+test("Valid login", async (t) => {
+  await t
+    .typeText("#username", "sagarkryadav31@gmail.com")
+    .typeText("#password", "Sagar$321")
+    .click("#login")
+});
+
+test("Invalid login", async (t) => {
+  await t
+    .typeText("#username", "incorrectUsername")
+    .typeText("#password", "incorrectPassword")
+    .click("#login")
+});
